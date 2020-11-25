7f71740676a41e603cd86884424eff43f6958ae0
KolobokTesto <dfilobok@omilia.com>
2020-11-25 19:55:13 +0200

    cosmetic update

--- src/test/java/DataHolder.java
+++ src/test/java/DataHolder.java
@@ -1,10 +1,9 @@
+import com.codeborne.selenide.Condition;
 import java.io.File;
-
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class DataHolder {
-
     public static String firstName = "Mike";
     public static String lastName = "Smith";
     public static String email = "jhon.smith@gmail.com";
@@ -16,6 +15,8 @@ public class DataHolder {
     public static String state = "Rajasthan" ;
     public static String city = "Jaipur" ;
     public static String subjects = "Maths" ;
+    public static String gender = "Male";
+    public static String hobby = "Reading";
 
     static void fillNameEmailMobAddress() {
         $("#firstName").setValue(firstName);
@@ -24,38 +25,36 @@ public class DataHolder {
         $("#userNumber").setValue(mobileNumber);
         $("#currentAddress").setValue(currentAddress);
     }
-
     static void fillSubject() {
         $("#subjectsInput").setValue(subjects).pressTab();
 
     }
-
     static void openPage() {
         open("https://demoqa.com/automation-practice-form");
     }
-
-    static void clickCheckboxes() {
-        $(".custom-control-label", 2).click();
-        $(".custom-control-label", 4).click();
+    static void selectGender() {
+        $$(byText(gender)).find(Condition.visible).click();
     }
-    static void fillDateOfBirth(){
+    static void selectHobby() {
+        $$(byText(hobby)).find(Condition.visible).click();
+    }
+    static void fillDateOfBirth() {
         $("#dateOfBirthInput").click();
         $(".react-datepicker__month-select").selectOption(monthOfBirth);
         $(".react-datepicker__year-select").selectOption(yearOfBirth);
         $(".react-datepicker__day--0"+dayOfBirth).click();
     }
-    static void fillStateCity(){
+    static void fillStateCity() {
         $("#state").scrollTo().click();
         $("#stateCity-wrapper").$(byText(state)).scrollTo().click();
         $("#city").scrollTo().click();
         $("#stateCity-wrapper").$(byText(city)).scrollTo().click();
     }
-    static void submit(){
+    static void submit() {
         $("#submit").click();
     }
-    static void uploadPicture(){
+    static void uploadPicture() {
         $("#uploadPicture").uploadFile(new File("src/test/resources/picture.txt"));
-
     }
 }
 
